573a366e9b2134d02bd9ab32baf01c23ea2e4015
lucas <bmfmlucas@gmail.com>
2024-11-14 16:22:48 -0300

    fix: imports

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Contatos.module.css'
 import { GoMail } from "react-icons/go";
-import { BsGithub, BsInstagram, BsLinkedin, BsYoutube } from 'react-icons/bs'
+import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
 
 function Contatos(){
     return(
